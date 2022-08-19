1e81449c859a4c773ec7b7c415b8322478f7d51c
unknown <kesha.bagadia@gmail.com>
2022-08-19 13:57:22 +0530

    1st commit

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseController : MonoBehaviour
+{
+    public UIManager uiManager = null;
+    public InputAction inputAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager.TogglePause();
+        inputAction.performed += unpause =>
+        {
+            StartCoroutine(unpauseRoutine());
+        };
+        inputAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator unpauseRoutine()
+    {
+        uiManager.TogglePause();
+        yield return new WaitForSeconds(0.1f);
+        uiManager.TogglePause();
+    }
+}
